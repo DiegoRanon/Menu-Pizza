86963569f3d672c40f8caf4c3adcc7ee5ac655e2
Konan <mastercelis8891@gmail.com>
2025-04-24 11:53:00 -0400

    Main Components

--- public/data.js
+++ public/data.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,58 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, and pepperoni</p>
-    </div>
-  );
-}
-
-function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
-}
-
-function Menu() {
-  return (
-    <div>
-      <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
-  );
-}
-
-function Footer() {
-  const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
-
-  if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
-
-  return (
-    <footer>{new Date().toLocaleDateString()}. We're currently open!</footer>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
